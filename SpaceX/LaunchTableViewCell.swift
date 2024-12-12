20c4a3d82c302d8eee298bfcc08d7bfb81a28b5a
Chandra Shekhar <chandukandhari@gmail.com>
2024-12-12 13:31:17 -0600

    Development of the app

---
+++ SpaceX/LaunchTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  LaunchTableViewCell.swift
+//  SpaceX
+//
+//  Created by Chandu .. on 12/11/24.
+//
+
+import UIKit
+
+class LaunchTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
